bbce2554bff8cbd5351c0c821bf3910e5553d4dd
AlexKwangwoo <bnc3049@gmail.com>
2020-10-21 05:59:59 -0600

    backend modified!!

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,6 +2,8 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
+    //뮤테이션중 createAccount이름을 빌려 함수를 정의함.. 실제로 만들떄는
+    // 밑에의 prisma.createUser와 같은 이미 정의된 함수를 써서 알려줘야함!
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
       const exists = await prisma.$exists.user({
@@ -16,6 +18,7 @@ export default {
         throw Error("This username / email is already taken");
       }
       await prisma.createUser({
+        // 프리즈마에 이미정의된 내용임!
         username,
         email,
         firstName,
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -5,7 +5,7 @@ export default {
     me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      return await prisma.user({ id: user.id });
+      return await prisma.user({ id: user.id }); //prisma.user는 User을 리턴함!
       //여기서 리턴을 user로 해줘서
       //프론트앤드에서 header쪽 me에서 user 입력없이 바로 username 이 가능함!
     },
